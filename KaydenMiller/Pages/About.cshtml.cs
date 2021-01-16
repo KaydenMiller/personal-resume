16b680cb06a2f68d9abcfea38253d60e13bf7033
Kayden Miller <kaydenm@gurutechnologies.net>
2021-01-15 21:37:11 -0700

    Initial Commit

---
+++ KaydenMiller/Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KaydenMiller.Pages
+{
+    public class About : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
