12ff1195f416cd33a8eca1fda23d3687b8489144
Kayden Miller <kaydenm@gurutechnologies.net>
2021-01-17 14:46:25 -0700

    Setup home page with general information

---
+++ KaydenMiller/Models/ProjectDisplayModel.cs
@@ -0,0 +1,12 @@
+﻿namespace KaydenMiller.Models
+{
+    public class ProjectDisplayModel
+    {
+        public string ProjectCarouselId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ButtonText { get; set; }
+        public string ImageUri { get; set; }
+        public string ImageAlt { get; set; }
+    }
+}
